9db522849e0c97f0365a527770cb60c9bd94b933
ChaseTerzian <chaseterzian@gmail.com>
2015-02-19 15:22:34 -0700

    crudding tasks, middle

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
     root "welcome#index"
+
     get "/terms" => "terms#terms"
     get "/about" => "about#about"
     get "/faq" => "common_questions#common_questions"
-    get "/tasks" => "tasks#tasks"
+    #get "/tasks" => "tasks#tasks"
+
+    resources :tasks
 end
