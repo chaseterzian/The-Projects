3768601c1d02d4d24e2cadd91223704f558221e8
ChaseTerzian <chaseterzian@gmail.com>
2015-02-22 23:19:36 -0700

    added bootstrap flash alert for create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,12 +11,13 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      flash[:created] = "Task was successfully created"
+      flash[:created] = ##message in application.html.erb
       redirect_to task_path(@task)
     else
       render :new
     end
   end
+
 
 
   def edit
