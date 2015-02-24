6c787d73d197db8d3b904df1dd9660c0c84a898c
ChaseTerzian <chaseterzian@gmail.com>
2015-02-23 17:59:00 -0700

    checkpoint, bootstrapped flash message

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      flash[:created] = ##message in application.html.erb
+      flash[:created] = "Task was successfully created"##message in application.html.erb
       redirect_to task_path(@task)
     else
       render :new
