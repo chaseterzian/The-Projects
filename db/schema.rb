2fa4443f241b813c1546f4cf515691bbd0b47529
ChaseTerzian <chaseterzian@gmail.com>
2015-02-28 16:06:30 -0700

    reformatted table to reflect actual data needed - first, last, email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227220735) do
+ActiveRecord::Schema.define(version: 20150228230233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,11 @@ ActiveRecord::Schema.define(version: 20150227220735) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
