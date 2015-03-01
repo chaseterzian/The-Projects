09d394565bb0e812fb0f9b7972e3d5d1b8564748
ChaseTerzian <chaseterzian@gmail.com>
2015-02-28 17:11:16 -0700

    users index properly formatted, index new create in controller, other user views empty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+
+  
 
 
 end
