3bfce28b2439dd1696e593ccca925c7d1e208da8
ChaseTerzian <chaseterzian@gmail.com>
2015-03-02 23:46:18 -0700

    Users new and users index almost complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
     if @user.save
       redirect_to users_path
     else
@@ -17,8 +17,38 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user = User.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :update
+    end
+  end
+
+  def destroy
+    User.destroy(params[:id])
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
   
+
+
+
+
 
 
 end
