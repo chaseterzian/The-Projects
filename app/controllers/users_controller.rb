7f22c3f2e3c4fb9fba1a913058142cbbbd6da3ed
ChaseTerzian <chaseterzian@gmail.com>
2015-03-06 02:20:12 -0700

    shared folder created for _errors, now used as a shared error partial for tasks users and projects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,16 +47,10 @@ class UsersController < ApplicationController
     end
   end
 
-    private
+  private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email)
   end
 
-
-
-
-
-
-
-  end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ActiveRecord::Base
+
+  validates :description, presence:true
+  #validates :due_date, presence:true
+
 end
