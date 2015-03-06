a081346f9f0136ca8cb4763470ff1fdd6e232358
ChaseTerzian <chaseterzian@gmail.com>
2015-03-06 00:28:27 -0700

    need to re-push to heroku to test users new and edit validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence:true
   validates :last_name, presence:true
   validates :email, presence:true
+  validates :email, uniqueness:true
 
 end
