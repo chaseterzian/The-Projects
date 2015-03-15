b387964cdf8acaa79d9ebc14768bfbc5e96cf7de
ChaseTerzian <chaseterzian@gmail.com>
2015-03-15 14:43:47 -0600

    Task-Nest-Assoc - tasks controller migrated and updated from gcamptest

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,39 +2,44 @@ class TasksController < ApplicationController
 
   before_filter :authenticate_user
 
+  before_action do
+    @project = Project.find(params[:project_id])
+  end
+
+
   def index
-    @tasks = Task.all
+    @tasks = @project.tasks.all
   end
 
   def new
-    @task = Task.new
+    @task = @project.tasks.new
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = @project.tasks.new(task_params)
     if @task.save
       flash[:message] = "Task was successfully created"
-      redirect_to task_path(@task)
+      redirect_to project_task_path(@project, @task)
     else
       render :new
     end
   end
 
   def edit
-    @task = Task.find(params[:id])
-    @task_edit_for_partial = Task.find(params[:id])
+    @task = @project.tasks.find(params[:id])
+    @task_edit_for_partial = @project.tasks.find(params[:id])
   end
 
   def show
-    @task = Task.find(params[:id])
-    @taskdate = Task.find(params[:id])
+    @task = @project.tasks.find(params[:id])
+    @taskdate = @project.tasks.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    @task = @project.tasks.find(params[:id])
     if @task.update(task_params)
       flash[:message] = "Task was successfully updated"
-      redirect_to task_path(@task)  ##removed s togoto show
+      redirect_to project_task_path(@project, @task)  ##removed s togoto show
     else
       render :edit
     end
@@ -44,7 +49,7 @@ class TasksController < ApplicationController
   def destroy
     Task.destroy(params[:id])
     flash[:message] = "Task was successfully deleted"
-    redirect_to tasks_path
+    redirect_to project_tasks_path(@project)
   end
 
 
@@ -52,7 +57,7 @@ class TasksController < ApplicationController
 
 
   def task_params
-    params.require(:task).permit(:description, :complete, :due_date)
+    params.require(:task).permit(:description, :complete, :due_date, :project_id)
   end
 
 end
