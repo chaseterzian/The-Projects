fb1e51ce3bdcfa33213bc9aee3b2e609936ee093
ChaseTerzian <chaseterzian@gmail.com>
2015-04-01 10:13:23 -0600

    gcamp link fixed for public pages

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -26,7 +26,7 @@ class MembershipsController < PrivateController
 
   def update
     @membership = Membership.find(params[:id])
-    if @membership.role == "Owner" && @project.memberships.where(role: "Owner").count <= 1 #####
+    if @membership.role == "Owner" && @project.memberships.where(role: "Owner").count == 1
       flash[:warning] = "Projects must have at least one owner"
       redirect_to project_memberships_path
     else
